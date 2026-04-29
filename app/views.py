d5f80dfae441f3843dea396e8bbd55edeec67991
jm653 <jmbvmoreira@gmail.com>
2026-04-29 10:40:35 -0300

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -1,70 +1,96 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views import View
 from django.contrib import messages
-from .models import Livro, Cidade, Autor, Editora, Leitor, Genero, Reserva
+from .models import *
+from django.views import View
+from .forms import LivroForm
 
-# ----------------------------
-# Views de página principal
-# ----------------------------
+
+
+
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'index.html')
+        livros = Livro.objects.all()
+        return render(request, 'index.html', {'livros': livros})
 
-# ----------------------------
-# Views de livros
-# ----------------------------
+
 class LivrosView(View):
     def get(self, request, *args, **kwargs):
         livros = Livro.objects.all()
         return render(request, 'livros.html', {'livros': livros})
-    
+
     def post(self, request, *args, **kwargs):
-        # Aqui você pode tratar submissão de formulário, se necessário
         pass
 
-class DeleteLivroView(View):
-    def get(self, request, id, *args, **kwargs):
-        # Busca o livro pelo ID ou retorna 404 se não existir
-        livro = get_object_or_404(Livro, id=id)
-        # Exclui o livro
-        livro.delete()
-        # Adiciona uma mensagem de sucesso
-        messages.success(request, 'Livro excluído com sucesso!')
-        # Redireciona para a lista de livros
-        return redirect('livros')
 
-# ----------------------------
-# Views de reservas
-# ----------------------------
-class ReservaView(View):
-    def get(self, request, *args, **kwargs):
-        reservas = Reserva.objects.all()
-        return render(request, 'reserva.html', {'reservas': reservas})
 
-# ----------------------------
-# Views de cadastro de entidades
-# ----------------------------
 class CidadesView(View):
     def get(self, request, *args, **kwargs):
         cidades = Cidade.objects.all()
         return render(request, 'cidade.html', {'cidades': cidades})
+
 
 class AutoresView(View):
     def get(self, request, *args, **kwargs):
         autores = Autor.objects.all()
         return render(request, 'autor.html', {'autores': autores})
 
+
 class EditorasView(View):
     def get(self, request, *args, **kwargs):
         editoras = Editora.objects.all()
         return render(request, 'editora.html', {'editoras': editoras})
+
 
 class LeitoresView(View):
     def get(self, request, *args, **kwargs):
         leitores = Leitor.objects.all()
         return render(request, 'leitor.html', {'leitores': leitores})
 
+
 class GenerosView(View):
     def get(self, request, *args, **kwargs):
         generos = Genero.objects.all()
         return render(request, 'genero.html', {'generos': generos})
+
+class DeleteLivroView(View):
+    def get(self, request, id, *args, **kwargs):
+        livro = get_object_or_404(Livro, id=id)
+        livro.delete()
+        messages.success(request, 'Livro excluído com sucesso!')
+        return redirect('index')
+
+    
+
+
+class EditarLivroView(View):
+    template_name = 'editar_livro.html'
+
+    def get(self, request, id, *args, **kwargs):
+        livro = get_object_or_404(Livro, id=id)
+        form = LivroForm(instance=livro)
+        return render(
+            request,
+            self.template_name,
+            {
+                'livro': livro,
+                'form': form
+            }
+        )
+
+    def post(self, request, id, *args, **kwargs):
+        livro = get_object_or_404(Livro, id=id)
+        form = LivroForm(request.POST, instance=livro)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'As edições foram salvas com sucesso.')
+            return redirect('index')  # Redirecionar de volta para a página de edição
+        else:
+            messages.error(request, 'Corrija os erros no formulário antes de enviar novamente.')
+            return render(
+                request,
+                self.template_name,
+                {
+                    'livro': livro,
+                    'form': form
+                }
+            )
--- config/settings.py
+++ config/settings.py
@@ -1,16 +1,13 @@
-# settings.py
 import os
+
 from pathlib import Path
 
-# Caminho base do projeto
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Segurança
 SECRET_KEY = 'django-insecure-8l8o(-2su$6t%k424293i#x672q0$*^itfyi9r32$8j-igo7zf'
+
 DEBUG = True
 ALLOWED_HOSTS = []
-
-# Aplicativos instalados
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -18,13 +15,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',  # seu app
+    'app',  # <-- nome da sua aplicação (onde está models.py)
 ]
 
-# Middleware
+
+
+#o template usado
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',  # corrigido typo
+    'django.contrib.sessions.middleware.SessionMiddleware',  # <-- corrigido
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -32,69 +31,61 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# URL principal
+
 ROOT_URLCONF = 'config.urls'
-
-# Templates
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'app/templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
+{
+'BACKEND':
+'django.template.backends.django.DjangoTemplates',
+'DIRS': [
+os.path.join(BASE_DIR, 'app/templates'),
+],
+'APP_DIRS': True,
+'OPTIONS': {
+'context_processors': [
+'django.template.context_processors.debug',
+'django.template.context_processors.request',
+'django.contrib.auth.context_processors.auth',
+'django.contrib.messages.context_processors.messages',
+],
+},
+},
 ]
-
-# WSGI
 WSGI_APPLICATION = 'config.wsgi.application'
-
-# Banco de dados PostgreSQL
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'biblioteca_n',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+'default': {
+'ENGINE': 'django.db.backends.postgresql',
+'NAME': 'bibliotecainfoh2026',
+'USER': 'postgres',
+'PASSWORD': '123456',
+'HOST': 'localhost',
+'PORT': '5432',
 }
-
-# Validação de senhas
+}
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+{
+'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+},
+{
+'NAME':
+'django.contrib.auth.password_validation.MinimumLengthValidator',
+},
+{
+'NAME':
+'django.contrib.auth.password_validation.CommonPasswordValidator',
+},
+{
+'NAME':
+'django.contrib.auth.password_validation.NumericPasswordValidator',
+},
 ]
-
-# Internacionalização
 LANGUAGE_CODE = 'pt-br'
 TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 USE_TZ = True
-
-# Arquivos estáticos
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # pasta onde os arquivos serão coletados
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(STATIC_URL, "static")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "app/static"),
+os.path.join(BASE_DIR, "app/static/"),
 ]
-
-# Chave primária padrão
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
